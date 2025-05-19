dd40d8ad8d4569ae29f16446f2b5a0436e75bbad
di0308 <di0308@naver.com>
2025-05-19 15:39:13 +0900

    fix: ui

--- src/components/MonitoringPanel.jsx
+++ src/components/MonitoringPanel.jsx
@@ -1,12 +1,8 @@
-import DashBoardChart from './charts/DashBoardChart';
 import PaymentLimitWebSocket from '../WebSocket/PaymentLimitWebSocket';
 
 const MonitoringPanel = () => {
   return (
     <div className="grid lg:grid-cols-2 gap-2">
-      <DashBoardChart title="누적 매출" multiLine namespace />
-      <DashBoardChart title="이상 결제 탐지" multiLine namespace />
-
       <PaymentLimitWebSocket/>
     </div>
   );
